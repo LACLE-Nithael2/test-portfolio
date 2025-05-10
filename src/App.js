d13c3db37f23a28c81f671998b241e2c8a06e28d
LACLE-Nithael2 <phvlacle@gmail.com>
2025-05-10 15:36:39 +0000

    premier commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Creative Greyscale Photography</h1>
+      <p>Portfolio Presentation</p>
+    </div>
+  );
+}
+
+export default App;
